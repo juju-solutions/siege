f1037aad09f1baf3cea0b7472d3757db34f819a2
Adam Israel <adam@adamisrael.com>
2015-03-23 11:44:31 -0600

    Integrate ch.contrib.benchmark

--- hooks/services.py
+++ hooks/services.py
@@ -2,53 +2,10 @@
 
 from charmhelpers.core.services.base import ServiceManager
 from charmhelpers.core.services import helpers
+from charmhelpers.contrib.benchmark import Benchmark
 
 import actions
 from charmhelpers.core import hookenv
-
-
-class Benchmark():
-    """
-    Helper class for the `benchmark` interface.
-
-    :param list actions: Define the actions that are also benchmarks
-
-    From inside the benchmark-relation-changed hook, you would
-    Benchmark(['memory', 'cpu', 'disk', 'smoke', 'custom'])
-
-    """
-
-    required_keys = [
-        'hostname',
-        'port',
-        'graphite_port',
-        'graphite_endpoint',
-        'api_port'
-    ]
-
-    def __init__(self, benchmarks=None):
-        if benchmarks is not None:
-            for rid in sorted(hookenv.relation_ids('benchmark')):
-                hookenv.relation_set(relation_id=rid, relation_settings={
-                    'benchmarks': ",".join(benchmarks)
-                })
-
-        # Check the relation data
-        config = {}
-        for key in self.required_keys:
-            val = hookenv.relation_get(key)
-            if val is not None:
-                config[key] = val
-            else:
-                # We don't have all of the required keys
-                config = {}
-                break
-
-        if len(config):
-            f = open('/etc/benchmark.conf', 'w')
-            for key, val in config.iteritems():
-                f.write("%s=%s\n" % (key, val))
-            f.close()
 
 
 class BenchmarkRelation(helpers.RelationContext):
@@ -68,15 +25,19 @@ class BenchmarkRelation(helpers.RelationContext):
 
     benchmarks = []
 
-    def __init__(self, benchmarks=None):
-        self.benchmarks = benchmarks
-        super(BenchmarkRelation, self).__init__(self.name, None)
+    def __init__(self, *args):
+        if args:
+            for arg in args:
+                hookenv.log("benchmark: %s" % arg)
+                self.benchmarks.append(arg)
+            Benchmark(self.benchmarks)
+
+        helpers.RelationContext.__init__(self, self.name)
 
     def is_ready(self):
         ready = super(BenchmarkRelation, self).is_ready()
         if ready and self.benchmarks:
             Benchmark(self.benchmarks)
-
         return ready
 
 
@@ -86,6 +47,9 @@ class HttpRelation(helpers.HttpRelation):
     provided by the relation, rather than the hard-coded port 80
     """
     required_keys = ['hostname', 'port']
+
+    def __init__(self, *args, **kwargs):
+        helpers.HttpRelation.__init__(self, *args, **kwargs)
 
     def provide_data(self):
         return {
@@ -100,17 +64,11 @@ def manage():
             'service': 'benchmark-siege',
             'ports': [],  # ports to after start
             'provided_data': [
-                # context managers for provided relations
-                # e.g.: helpers.HttpRelation()
-                # BenchmarkRelation(['list', 'of', 'actions1']),
+                BenchmarkRelation('siege'),
             ],
             'required_data': [
-                # data (contexts) required to start the service
-                # e.g.: helpers.RequiredConfig('domain', 'auth_key'),
-                #       helpers.MysqlRelation(),
-                # helpers.RequiredConfig('hostname', 'port'),
                 HttpRelation(),
-                BenchmarkRelation(['siege']),
+                BenchmarkRelation(),
             ],
             'data_ready': [
                 helpers.render_template(
